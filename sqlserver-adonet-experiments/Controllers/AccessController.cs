e0c89106c8f8a4cb576917aa804e1daf527de0e3
bsaranga <bsaranga@gmail.com>
2023-03-31 11:05:23 +0530

    initalized new web api

---
+++ sqlserver-adonet-experiments/Controllers/AccessController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sqlserver_adonet_experiments.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccessController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Test()
+        {
+            return Ok(new
+            {
+                Result = "Okay"
+            });
+        }
+    }
+}
